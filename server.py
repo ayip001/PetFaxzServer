3d3e6b677e089d9435d5927b6315b3e719f190a2
twof <fabiobean2@gmail.com>
2017-05-14 00:21:51 -0700

    Added some messages and random selection

--- server.py
+++ server.py
@@ -1,8 +1,16 @@
 from flask import Flask, request
+import random
 import json
 import csv
 
 app = Flask(__name__)
+
+messages = [("Princess Di lived to be 36 and so has {name}. {name} has " +
+             "{time_left} hours left before {pronoun} experiences" +
+             " statistical death."),
+            ("{name} is going through a mid life crisis. {pronoun} has" +
+             " {time_left} left to live")
+            ]
 
 
 @app.route("/generatemsg")
@@ -11,8 +19,17 @@ def main():
     age = int(request.args.get('age'))
     name = str(request.args.get('name'))
     pronoun = str(request.args.get('pronoun'))
+
     # dead = str(request.args.get('dead'))
-    return json.dumps({"breed": breed, "age": age, "name": name, "pronoun": pronoun})
+
+    time_left = 0
+
+    message = random.choice(messages).format(name=name,
+                                             time_left=time_left,
+                                             pronoun=pronoun)
+
+    return json.dumps({"message": message})
+
 
 if __name__ == "__main__":
     app.run()
