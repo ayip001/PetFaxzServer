9c414a4d641e66c47a4bc52be40fd5f851c719b9
Angus Yip <Angus Yip>
2017-05-13 23:42:58 -0700

    reading csv ok

---
+++ README.md
@@ -0,0 +1,3 @@
+# backend server support for Pet Faxz
+## Stupid hackathon 2017
+https://github.com/twof/PetFaxz
--- server.py
+++ server.py
@@ -1,9 +1,15 @@
 from flask import Flask, request
 import json
 import csv
+import sys
 
 app = Flask(__name__)
 
+def getLifeSpan( breed ):
+    csv_file = csv.reader(open('data.csv', "rb"), delimiter=",")
+    for row in csv_file:
+        if breed == row[0]:
+             return int(row[1])
 
 @app.route("/generatemsg")
 def main():
@@ -12,7 +18,9 @@ def main():
     name = str(request.args.get('name'))
     pronoun = str(request.args.get('pronoun'))
     # dead = str(request.args.get('dead'))
-    return json.dumps({"breed": breed, "age": age, "name": name, "pronoun": pronoun})
+
+    span = getLifeSpan( breed )
+    return json.dumps({"breed": breed, "age": age, "name": name, "pronoun": pronoun, "span": span})
 
 if __name__ == "__main__":
     app.run()
